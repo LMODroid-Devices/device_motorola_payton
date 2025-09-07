1ae4c1a16dc187e58082f1860ca2617c76c6cf33
LMO GerritBot <gerrit@libremobileos.com>
2025-09-07 13:06:26 +0000

    Adapt to LMODroid by device-add script

--- lmodroid_payton.mk
+++ lmodroid_payton.mk
@@ -1,6 +1,6 @@
 # Inherit some common Lineage stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Device
@@ -11,7 +11,7 @@ PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := payton
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_MODEL := Moto X4
-PRODUCT_NAME := lineage_payton
+PRODUCT_NAME := lmodroid_payton
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="payton-user 9 PPW29.69-40-4 4ca2a release-keys" \
